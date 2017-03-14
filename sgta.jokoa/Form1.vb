f7addb533683b1eb090bb88c333c7d8ea397b48a
Mielotxim <ekaneiza8@gmail.com>
2017-03-14 22:02:05 +0100

    no se que es esto

--- sgta.jokoa/Form1.vb
+++ sgta.jokoa/Form1.vb
@@ -3,4 +3,8 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub Jokatu_Click(sender As Object, e As EventArgs) Handles Jokatu.Click
+
+    End Sub
 End Class

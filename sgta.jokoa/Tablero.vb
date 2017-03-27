35f4e831d28cbbef6e901680b1602a6ec03e6a5a
Mielotxim <ekaneiza8@gmail.com>
2017-03-27 14:53:09 +0200

    hola don pepito....

--- sgta.jokoa/Tablero.vb
+++ sgta.jokoa/Tablero.vb
@@ -1,14 +1,20 @@
 ﻿Public Class Tablero
 
     Private Sub Tablero_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        For I As Integer = 1 To 10
+        Dim z As Integer
+        z = 1
+        For I As Integer = 1 To 25
             Dim b As New Button()
+            If z = 5 Then
+                z = 1
+            End If
             b.Text = "Texto " & I
             b.Height = Me.Height / 10
             b.Left = botoiaJarri(I)
             b.Width = 100
-            b.Top = (I - 1) * (b.Height)
+            b.Top = z * (b.Height)
             Me.Controls.Add(b)
+            z = z + 1
         Next I
     End Sub
 

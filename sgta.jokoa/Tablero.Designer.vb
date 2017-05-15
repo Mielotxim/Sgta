3368e39e63b36c2a0b3fcf8b37bc3f18c06b47b1
Mielotxim <ekaneiza8@gmail.com>
2017-05-15 11:44:39 +0200

    El codigo del tablero esta pasado al de verdad

--- sgta.jokoa/Tablero.Designer.vb
+++ sgta.jokoa/Tablero.Designer.vb
@@ -23,7 +23,92 @@ Partial Class Tablero
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Tablero))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Button4)
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(213, 592)
+        Me.Panel1.TabIndex = 0
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(16, 293)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(16, 245)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(16, 200)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(16, 161)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 123)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Label1"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(184, 104)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Panel2
+        '
+        Me.Panel2.Location = New System.Drawing.Point(220, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(1238, 592)
+        Me.Panel2.TabIndex = 1
         '
         'Tablero
         '
@@ -31,11 +116,24 @@ Partial Class Tablero
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoScroll = True
         Me.BackgroundImage = Global.sgta.jokoa.My.Resources.Resources.Plain_Background
-        Me.ClientSize = New System.Drawing.Size(451, 394)
+        Me.ClientSize = New System.Drawing.Size(1239, 608)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Tablero"
         Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
 End Class
--- sgta.jokoa/Tablero.vb
+++ sgta.jokoa/Tablero.vb
@@ -1,35 +1,43 @@
 ﻿Public Class Tablero
-    Dim aux(23, 45) As Button
-    Dim aldaketa As Boolean
+    Dim aux(100, 100) As PictureBox
     Private Sub Tablero_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        For altuera As Integer = 1 To 12
-            For zabalera As Integer = 1 To 23
-                Dim b As New Button()
+        For altuera As Integer = 1 To 10
+            For zabalera As Integer = 1 To 10
+                Dim b As New PictureBox()
                 b.Height = 40
                 b.Width = 40
-                b.Left = 40 * zabalera + 10
+                b.Left = 40 * zabalera
                 b.Top = 40 * altuera - 10
-
+                b.BorderStyle = BorderStyle.FixedSingle
+                b.Name = altuera.ToString
+                AddHandler b.Click, AddressOf OnbClick
                 If zabalera = 14 Or zabalera = 13 Or zabalera = 15 Then
                     b.BackgroundImage = sgta.jokoa.My.Resources.water
                 Else
                     b.BackgroundImage = sgta.jokoa.My.Resources.floresiñas
                 End If
                 aux(altuera, zabalera) = b
-                Me.Controls.Add(b)
+                Panel2.Controls.Add(b)
+
             Next
         Next
+
+        Label1.Text = "TU PERSONAJE"
+
+        Button1.Text = "Gulag"
+
     End Sub
 
-    Private Sub clikatuta(zabalera As Integer, altuera As Integer)
-        If aldaketa Then
-            aux(zabalera, altuera).BackgroundImage = sgta.jokoa.My.Resources.water
-            altuera = False
-        Else
-            aux(zabalera, altuera).BackgroundImage = sgta.jokoa.My.Resources.water
-            altuera = True
-        End If
+    Private Sub OnbClick(ByVal sender As Object, ByVal e As MouseEventArgs)
+        sender.BackgroundImage = sgta.jokoa.My.Resources.water
+        Label1.Text = sender.name
+        Dim a As Integer
+        a = sender.name
+        aux(a, a).BackgroundImage = sgta.jokoa.My.Resources.water
+    End Sub
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        aux(10, 10).BackgroundImage = sgta.jokoa.My.Resources.water
     End Sub
 
 End Class

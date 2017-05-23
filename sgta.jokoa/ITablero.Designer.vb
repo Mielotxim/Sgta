21bcd294e268871abb337b1e109d2da0fecf9331
Oier Beaskoetxea <obeaskoetxea@gmail.com>
2017-05-23 13:25:16 +0200

    metodo para dibujar el rombo del alcance hecho (faltaria solo poner los colores) y cambios en la interfaz de juego para prepararla y poder camiar de fases, ver datos, etc.

--- sgta.jokoa/ITablero.Designer.vb
+++ sgta.jokoa/ITablero.Designer.vb
@@ -24,80 +24,304 @@ Partial Class ITablero
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ITablero))
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Button4 = New System.Windows.Forms.Button()
+        Me.lblTxanda = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.lblFase = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblHp = New System.Windows.Forms.Label()
+        Me.lblAtk = New System.Windows.Forms.Label()
+        Me.lblMov = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.lblTeam = New System.Windows.Forms.Label()
+        Me.lblDef = New System.Windows.Forms.Label()
+        Me.lblAlk = New System.Windows.Forms.Label()
+        Me.btnCancel = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.Label1 = New System.Windows.Forms.Label()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Panel2 = New System.Windows.Forms.Panel()
         Me.Panel1.SuspendLayout()
+        Me.TableLayoutPanel1.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Panel1
         '
-        Me.Panel1.Controls.Add(Me.Button4)
+        Me.Panel1.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.Panel1.Controls.Add(Me.lblTxanda)
+        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Controls.Add(Me.Button5)
+        Me.Panel1.Controls.Add(Me.lblFase)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.TableLayoutPanel1)
+        Me.Panel1.Controls.Add(Me.btnCancel)
         Me.Panel1.Controls.Add(Me.Button3)
         Me.Panel1.Controls.Add(Me.Button2)
         Me.Panel1.Controls.Add(Me.Button1)
-        Me.Panel1.Controls.Add(Me.Label1)
         Me.Panel1.Controls.Add(Me.PictureBox1)
-        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Location = New System.Drawing.Point(-1, -14)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(213, 592)
+        Me.Panel1.Size = New System.Drawing.Size(214, 612)
         Me.Panel1.TabIndex = 0
         '
-        'Button4
+        'lblTxanda
         '
-        Me.Button4.Location = New System.Drawing.Point(16, 293)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(75, 23)
-        Me.Button4.TabIndex = 6
-        Me.Button4.Text = "Button4"
-        Me.Button4.UseVisualStyleBackColor = True
+        Me.lblTxanda.AutoSize = True
+        Me.lblTxanda.Location = New System.Drawing.Point(80, 304)
+        Me.lblTxanda.Name = "lblTxanda"
+        Me.lblTxanda.Size = New System.Drawing.Size(53, 13)
+        Me.lblTxanda.TabIndex = 12
+        Me.lblTxanda.Text = "lblTxanda"
         '
-        'Button3
+        'Label7
         '
-        Me.Button3.Location = New System.Drawing.Point(16, 245)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 23)
-        Me.Button3.TabIndex = 5
-        Me.Button3.Text = "Button3"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(17, 304)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(57, 13)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "TXANDA: "
         '
-        'Button2
+        'Button5
         '
-        Me.Button2.Location = New System.Drawing.Point(16, 200)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 4
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button5.Location = New System.Drawing.Point(117, 556)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(80, 23)
+        Me.Button5.TabIndex = 10
+        Me.Button5.Text = "Button5"
+        Me.Button5.UseVisualStyleBackColor = True
         '
-        'Button1
+        'lblFase
         '
-        Me.Button1.Location = New System.Drawing.Point(16, 161)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 3
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.lblFase.AutoSize = True
+        Me.lblFase.Location = New System.Drawing.Point(80, 331)
+        Me.lblFase.Name = "lblFase"
+        Me.lblFase.Size = New System.Drawing.Size(40, 13)
+        Me.lblFase.TabIndex = 9
+        Me.lblFase.Text = "lblFase"
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(13, 123)
+        Me.Label1.Location = New System.Drawing.Point(17, 331)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
-        Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "FASE: "
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 4
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 21.85792!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.3224!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 23.49727!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.3224!))
+        Me.TableLayoutPanel1.Controls.Add(Me.lblName, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label2, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label3, 0, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label4, 0, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblHp, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblAtk, 1, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblMov, 1, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label8, 2, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label9, 2, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label10, 2, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblTeam, 3, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblDef, 3, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblAlk, 3, 3)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(14, 128)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 4
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(183, 161)
+        Me.TableLayoutPanel1.TabIndex = 7
+        Me.TableLayoutPanel1.Visible = False
+        '
+        'lblName
+        '
+        Me.lblName.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.lblName.AutoSize = True
+        Me.TableLayoutPanel1.SetColumnSpan(Me.lblName, 4)
+        Me.lblName.Location = New System.Drawing.Point(3, 13)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(45, 13)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "lblName"
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(3, 53)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(28, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "HP: "
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(3, 93)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(31, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "ATK: "
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(3, 127)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(31, 26)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "MOV: "
+        '
+        'lblHp
+        '
+        Me.lblHp.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.lblHp.AutoSize = True
+        Me.lblHp.Location = New System.Drawing.Point(42, 53)
+        Me.lblHp.Name = "lblHp"
+        Me.lblHp.Size = New System.Drawing.Size(39, 13)
+        Me.lblHp.TabIndex = 4
+        Me.lblHp.Text = "Label5"
+        '
+        'lblAtk
+        '
+        Me.lblAtk.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.lblAtk.AutoSize = True
+        Me.lblAtk.Location = New System.Drawing.Point(42, 93)
+        Me.lblAtk.Name = "lblAtk"
+        Me.lblAtk.Size = New System.Drawing.Size(39, 13)
+        Me.lblAtk.TabIndex = 5
+        Me.lblAtk.Text = "Label6"
+        '
+        'lblMov
+        '
+        Me.lblMov.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.lblMov.AutoSize = True
+        Me.lblMov.Location = New System.Drawing.Point(42, 134)
+        Me.lblMov.Name = "lblMov"
+        Me.lblMov.Size = New System.Drawing.Size(39, 13)
+        Me.lblMov.TabIndex = 6
+        Me.lblMov.Text = "Label7"
+        '
+        'Label8
+        '
+        Me.Label8.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(91, 53)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(37, 13)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Team: "
+        '
+        'Label9
+        '
+        Me.Label9.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(91, 93)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(34, 13)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "DEF: "
+        '
+        'Label10
+        '
+        Me.Label10.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(91, 134)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(33, 13)
+        Me.Label10.TabIndex = 9
+        Me.Label10.Text = "ALK: "
+        '
+        'lblTeam
+        '
+        Me.lblTeam.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.lblTeam.AutoSize = True
+        Me.lblTeam.Location = New System.Drawing.Point(134, 53)
+        Me.lblTeam.Name = "lblTeam"
+        Me.lblTeam.Size = New System.Drawing.Size(45, 13)
+        Me.lblTeam.TabIndex = 10
+        Me.lblTeam.Text = "Label11"
+        '
+        'lblDef
+        '
+        Me.lblDef.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.lblDef.AutoSize = True
+        Me.lblDef.Location = New System.Drawing.Point(134, 93)
+        Me.lblDef.Name = "lblDef"
+        Me.lblDef.Size = New System.Drawing.Size(45, 13)
+        Me.lblDef.TabIndex = 11
+        Me.lblDef.Text = "Label12"
+        '
+        'lblAlk
+        '
+        Me.lblAlk.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.lblAlk.AutoSize = True
+        Me.lblAlk.Location = New System.Drawing.Point(134, 134)
+        Me.lblAlk.Name = "lblAlk"
+        Me.lblAlk.Size = New System.Drawing.Size(45, 13)
+        Me.lblAlk.TabIndex = 12
+        Me.lblAlk.Text = "Label13"
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(20, 556)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(83, 23)
+        Me.btnCancel.TabIndex = 6
+        Me.btnCancel.Text = "EZEZTATU"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(14, 451)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(184, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Txanda Bukatu"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(14, 412)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(184, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Eraso Fasea"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(14, 373)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(184, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Mugimendu Fasea"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'PictureBox1
         '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Black
         Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Location = New System.Drawing.Point(13, 17)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(184, 104)
         Me.PictureBox1.TabIndex = 1
@@ -110,7 +334,7 @@ Partial Class ITablero
         Me.Panel2.Size = New System.Drawing.Size(1238, 592)
         Me.Panel2.TabIndex = 1
         '
-        'Tablero
+        'ITablero
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
@@ -120,20 +344,40 @@ Partial Class ITablero
         Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Panel1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.Name = "Tablero"
+        Me.Name = "ITablero"
         Me.Text = "Form1"
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents Panel1 As System.Windows.Forms.Panel
-    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
     Friend WithEvents Button3 As System.Windows.Forms.Button
     Friend WithEvents Button2 As System.Windows.Forms.Button
     Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
     Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents lblHp As System.Windows.Forms.Label
+    Friend WithEvents lblAtk As System.Windows.Forms.Label
+    Friend WithEvents lblMov As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents lblTeam As System.Windows.Forms.Label
+    Friend WithEvents lblDef As System.Windows.Forms.Label
+    Friend WithEvents lblAlk As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblFase As System.Windows.Forms.Label
+    Friend WithEvents lblTxanda As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Button5 As System.Windows.Forms.Button
 End Class
--- sgta.jokoa/ITablero.vb
+++ sgta.jokoa/ITablero.vb
@@ -1,8 +1,8 @@
 ﻿Imports sgta.logika
 
 Public Class ITablero
-    'Dim aux(Sistema.getTable().getAltuera, Sistema.getTable().getZabalera) As PictureBox
-    Dim aux(100, 100) As PictureBox
+    'Dim taula(Sistema.getTable().getAltuera, Sistema.getTable().getZabalera) As PictureBox
+    Dim taula(100, 100) As PictureBox
     Private Sub Tablero_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         For altuera As Integer = 1 To 10
             For zabalera As Integer = 1 To 10
@@ -19,27 +19,50 @@ Public Class ITablero
                 Else
                     b.BackgroundImage = sgta.jokoa.My.Resources.floresiñas
                 End If
-                aux(altuera, zabalera) = b
+                taula(altuera, zabalera) = b
                 Panel2.Controls.Add(b)
             Next
         Next
 
-        Label1.Text = "TU PERSONAJE"
-
+        lblName.Text = "TU PERSONAJE"
+        
         Button1.Text = "Gulag"
-
     End Sub
 
     Private Sub OnbClick(ByVal sender As Object, ByVal e As MouseEventArgs)
         sender.BackgroundImage = sgta.jokoa.My.Resources.water
-        Label1.Text = sender.name
+        lblName.Text = sender.name
         Dim a As Integer
         a = sender.name
-        aux(a, a).BackgroundImage = sgta.jokoa.My.Resources.water
+        taula(a, a).BackgroundImage = sgta.jokoa.My.Resources.water
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        aux(10, 10).BackgroundImage = sgta.jokoa.My.Resources.water
+        taula(10, 10).BackgroundImage = sgta.jokoa.My.Resources.water
     End Sub
 
+    Private Sub setColorRed(ByVal altuera As Integer, ByVal zabalera As Integer, ByVal dis As Integer)
+        'hay que averiguar como cambiar el color (o poner una nueva imagen en su lugar)
+        Dim auxZ As Integer = 0
+        Dim auxA As Integer = dis
+        While auxA >= 0
+            If Not (auxA = 0) Then
+                If (altuera - auxA >= 0) Then
+                    taula(altuera - auxA, zabalera).BackColor = Color.Red
+                End If
+                If (altuera + auxA <= Sistema.getTable.getAltuera()) Then
+                    taula(altuera + auxA, zabalera).BackColor = Color.Red
+                End If
+            End If
+            While (auxA + auxZ <= dis)
+                taula(altuera + auxA, zabalera - auxZ).BackColor = Color.Red
+                taula(altuera + auxA, zabalera + auxZ).BackColor = Color.Red
+                taula(altuera - auxA, zabalera - auxZ).BackColor = Color.Red
+                taula(altuera - auxA, zabalera + auxZ).BackColor = Color.Red
+                auxZ += 1
+            End While
+            auxZ = 0
+            auxA -= 1
+        End While
+    End Sub
 End Class

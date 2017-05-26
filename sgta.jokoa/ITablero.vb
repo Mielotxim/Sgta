ac854059522e68f61621ad7d64349fcb910b7bb6
Mielotxim <ekaneiza8@gmail.com>
2017-05-26 07:46:18 +0200

    better acuracy in the tablero

--- sgta.jokoa/ITablero.vb
+++ sgta.jokoa/ITablero.vb
@@ -4,15 +4,15 @@ Public Class ITablero
     'Dim taula(Sistema.getTable().getAltuera, Sistema.getTable().getZabalera) As PictureBox
     Dim taula(100, 100) As PictureBox
     Private Sub Tablero_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        For altuera As Integer = 1 To 10
-            For zabalera As Integer = 1 To 10
+        For altuera As Integer = 1 To 20
+            For zabalera As Integer = 1 To 20
                 Dim b As New PictureBox()
                 b.Height = 40
                 b.Width = 40
                 b.Left = 40 * zabalera
                 b.Top = 40 * altuera - 10
                 b.BorderStyle = BorderStyle.FixedSingle
-                b.Name = altuera.ToString
+                b.Name = altuera.ToString + "/" + zabalera.ToString
                 AddHandler b.Click, AddressOf OnbClick
                 If zabalera = 14 Or zabalera = 13 Or zabalera = 15 Then
                     b.BackgroundImage = sgta.jokoa.My.Resources.water
@@ -26,15 +26,14 @@ Public Class ITablero
 
         lblName.Text = "TU PERSONAJE"
         
-        Button1.Text = "Gulag"
     End Sub
 
     Private Sub OnbClick(ByVal sender As Object, ByVal e As MouseEventArgs)
-        sender.BackgroundImage = sgta.jokoa.My.Resources.water
-        lblName.Text = sender.name
-        Dim a As Integer
+        Dim a As String
+        Dim b(2) As String
         a = sender.name
-        taula(a, a).BackgroundImage = sgta.jokoa.My.Resources.water
+        b = a.Split("/")
+        taula(b(0), b(1)).BackgroundImage = sgta.jokoa.My.Resources.water
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
